d1d48da824ef4af8770ba438372467a3f7dfab44
DE YU <71481700+Delsin-Yu@users.noreply.github.com>
2025-05-30 17:06:11 +0800

    Support multi midi track chunk

--- MidiFlattener/Program.cs
+++ MidiFlattener/Program.cs
@@ -8,36 +8,39 @@ var dir = args[0];
 
 foreach (var file in Directory.GetFiles(dir, "*.mid", SearchOption.AllDirectories))
 {
-    if(file.Contains(extension)) continue;
-    
-    var sourceMidiFile = MidiFile.Read(file);
+    if (file.Contains(extension)) continue;
 
-    var sourceMidiTrack =
-        sourceMidiFile
-            .Chunks
-            .OfType<TrackChunk>()
-            .First(track => track.GetNotes().Count != 0);
+    var sourceMidiFile = MidiFile.Read(file);
 
     var targetTempo = Tempo.FromBeatsPerMinute(targetTempoValue);
     var dstTempoMap = TempoMap.Create(targetTempo);
-    
     var sourceTempoMap = sourceMidiFile.GetTempoMap();
-    var noteTime = sourceMidiTrack
-        .GetNotes()
-        .Select(x => (
-            x.NoteName,
-            x.Octave,
-            x.Velocity,
-            x.OffVelocity,
-            Time: x.TimeAs<MetricTimeSpan>(sourceTempoMap),
-            Length: x.LengthAs<MetricTimeSpan>(sourceTempoMap))
-        ).ToList();
 
-    var timedEvents = sourceMidiTrack
-        .GetTimedEvents()
-        .Where(x => x.Event is ControlChangeEvent)
-        .Select(
-            x =>
+    var midiTrackList = new List<MidiChunk>
+    {
+        new TrackChunk(
+            new TimeSignatureEvent(4, 4),
+            new SetTempoEvent(targetTempo.MicrosecondsPerQuarterNote)
+        )
+    };
+
+    foreach (var sourceMidiTrack in sourceMidiFile.Chunks.OfType<TrackChunk>())
+    {
+        var noteTime = sourceMidiTrack
+            .GetNotes()
+            .Select(x => (
+                x.NoteName,
+                x.Octave,
+                x.Velocity,
+                x.OffVelocity,
+                Time: x.TimeAs<MetricTimeSpan>(sourceTempoMap),
+                Length: x.LengthAs<MetricTimeSpan>(sourceTempoMap))
+            ).ToList();
+
+        var timedEvents = sourceMidiTrack
+            .GetTimedEvents()
+            .Where(x => x.Event is ControlChangeEvent)
+            .Select(x =>
             {
                 var cc = (ControlChangeEvent)x.Event;
                 return (
@@ -46,46 +49,42 @@ foreach (var file in Directory.GetFiles(dir, "*.mid", SearchOption.AllDirectorie
                     Time: x.TimeAs<MetricTimeSpan>(sourceTempoMap)
                 );
             });
-    
-    var midiTrack = new TrackChunk();
-    
-    var newMidi = new MidiFile(
-        new TrackChunk(
-            new TimeSignatureEvent(4, 4),
-            new SetTempoEvent(targetTempo.MicrosecondsPerQuarterNote)
-        ),
-        midiTrack
-    );
 
-    var noteManager = midiTrack.ManageNotes();
-    foreach (var data in noteTime)
-    {
-        var time = TimeConverter.ConvertFrom(data.Time, dstTempoMap);
-        var length = TimeConverter.ConvertFrom(data.Length, dstTempoMap);
-        
-        var note = new Note(
-            data.NoteName,
-            data.Octave,
-            length,
-            time
-        )
+        var midiTrack = new TrackChunk();
+        midiTrackList.Add(midiTrack);
+
+        var noteManager = midiTrack.ManageNotes();
+        foreach (var data in noteTime)
         {
-            Velocity = data.Velocity,
-            OffVelocity = data.OffVelocity
-        };
+            var time = TimeConverter.ConvertFrom(data.Time, dstTempoMap);
+            var length = TimeConverter.ConvertFrom(data.Length, dstTempoMap);
 
-        noteManager.Objects.Add(note);
-    }
-    noteManager.SaveChanges();
+            var note = new Note(
+                data.NoteName,
+                data.Octave,
+                length,
+                time
+            )
+            {
+                Velocity = data.Velocity,
+                OffVelocity = data.OffVelocity
+            };
 
-    var eventManager = midiTrack.ManageTimedEvents();
-    foreach (var data in timedEvents)
-    {
-        var time = TimeConverter.ConvertFrom(data.Time, dstTempoMap);
-        var cc = new ControlChangeEvent(data.ControlNumber, data.ControlValue);
-        eventManager.Objects.Add(new TimedEvent(cc, time));
+            noteManager.Objects.Add(note);
+        }
+
+        noteManager.SaveChanges();
+
+        var eventManager = midiTrack.ManageTimedEvents();
+        foreach (var data in timedEvents)
+        {
+            var time = TimeConverter.ConvertFrom(data.Time, dstTempoMap);
+            var cc = new ControlChangeEvent(data.ControlNumber, data.ControlValue);
+            eventManager.Objects.Add(new TimedEvent(cc, time));
+        }
+
+        eventManager.SaveChanges();
     }
-    eventManager.SaveChanges();
-    
-    newMidi.Write(Path.ChangeExtension(file, extension), true);
+
+    new MidiFile(midiTrackList).Write(Path.ChangeExtension(file, extension), true);
 }
